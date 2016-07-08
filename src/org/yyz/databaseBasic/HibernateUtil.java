7bab947f662cbde842a75e1d7e8c24cc2d7ec8b8
Yangyazheng <2017948437@qq.com>
2016-07-08 19:26:29 +0800

    hibernate util to access and use database

--- src/org/yyz/databaseBasic/HibernateUtil.java
+++ src/org/yyz/databaseBasic/HibernateUtil.java
@@ -1,52 +1,127 @@
 package org.yyz.databaseBasic;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.internal.SessionFactoryImpl;
-import org.hibernate.proxy.HibernateProxy;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 /**
  * Created by yyz on 2016/7/6.
  */
 public class HibernateUtil {
-    private static SessionFactory sessionFactory;
-    static{
-        try{
-            //读取hibernate.cfg.xml文件
-            Configuration cfg=new Configuration().configure();
 
-            //建立SessionFactory
-            sessionFactory=cfg.buildSessionFactory();
+    /**
+     * Location of hibernate.cfg.xml file.
+     * Location should be on the classpath as Hibernate uses
+     * #resourceAsStream style lookup for its configuration file.
+     * The default classpath location of the hibernate config file is
+     * in the default package. Use #setConfigFile() to update
+     * the location of the configuration file for the current session.
+     */
+    private static final ThreadLocal<Session> threadLocal = new ThreadLocal<Session>();
+    private static org.hibernate.SessionFactory sessionFactory;
 
-        }catch(Exception e){
+    private static Configuration configuration = new Configuration();
+    private static ServiceRegistry serviceRegistry;
+
+    static {
+        try {
+            configuration.configure("org/yyz/pojo/hibernate.cfg.xml");
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception e) {
+            System.err.println("%%%% Error Creating SessionFactory %%%%");
             e.printStackTrace();
         }
     }
+    private HibernateUtil() {
+    }
 
     /**
-     * 开启session
-     * @return
+     * Returns the ThreadLocal Session instance.  Lazy initialize
+     * the <code>SessionFactory</code> if needed.
+     *
+     *  @return Session
+     *  @throws HibernateException
      */
-    public static Session getSession(){
-        Session session = sessionFactory.openSession();
+    public static Session getSession() throws HibernateException {
+        Session session = (Session) threadLocal.get();
+
+        if (session == null || !session.isOpen()) {
+            if (sessionFactory == null) {
+                rebuildSessionFactory();
+            }
+            session = (sessionFactory != null) ? sessionFactory.openSession()
+                    : null;
+            threadLocal.set(session);
+        }
 
         return session;
     }
 
     /**
-     * 关闭session事务
-     * @param session
+     *  Rebuild hibernate session factory
+     *
      */
-    public static void closeSession(Session session){
-        if(session!=null){
-            if(session.isOpen()){
-                session.close();
-            }
+    public static void rebuildSessionFactory() {
+        try {
+            configuration.configure();
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception e) {
+            System.err.println("%%%% Error Creating SessionFactory %%%%");
+            e.printStackTrace();
         }
     }
 
-    public static SessionFactory getSessionFactory(){
+    /**
+     *  Close the single hibernate session instance.
+     *
+     *  @throws HibernateException
+     */
+    public static void closeSession() throws HibernateException {
+        Session session = (Session) threadLocal.get();
+        threadLocal.set(null);
+
+        if (session != null) {
+            session.close();
+        }
+    }
+
+    /**
+     *  return session factory
+     *
+     */
+    public static org.hibernate.SessionFactory getSessionFactory() {
         return sessionFactory;
+    }
+    /**
+     *  return hibernate configuration
+     *
+     */
+    public static Configuration getConfiguration() {
+        return configuration;
+    }
+
+    /**
+     * start a transaction
+     * @param session
+     */
+    public static void begin(Session session){
+        if(null != session)
+            session.getTransaction().begin();
+    }
+
+    /**
+     * commit operation of DB
+     * @param session
+     */
+    public static void commit(Session session){
+        if(null != session){
+            session.flush();
+            session.getTransaction().commit();
+        }
     }
 }

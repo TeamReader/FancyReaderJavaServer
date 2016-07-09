750baf1e79d3b2653f876a244ebc6f2e93e96588
Yangyazheng <2017948437@qq.com>
2016-07-09 15:20:42 +0800

    Boolean result bean

---
+++ src/org/yyz/bean/BooleanResultBean.java
@@ -0,0 +1,17 @@
+package org.yyz.bean;
+
+/**
+ * Created by yyz on 2016/7/6.
+ */
+public class BooleanResultBean {
+    private String result;
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getResult() {
+
+        return result;
+    }
+}
